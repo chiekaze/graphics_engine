c3ab87bda36dff57b1ad15ab2d0070758a4f2ad2
chiekaze <juho.vaihoja@outlook.com>
2016-09-05 13:24:44 +0300

    yes

---
+++ TestDemo/TestDemo.cpp
@@ -0,0 +1,39 @@
+// TestDemo.cpp : Defines the entry point for the console application.
+//
+
+#include "Object.h"
+#include "Ref.h"
+#include "ElapsedTimer.h"
+#include <iostream>
+
+//K‰ytet‰‰n engine -namespacea
+using namespace engine;
+
+int doSomeStuff()
+{
+	int a = 0.0;
+
+	for (int i = 0; i < 10000000; ++i)
+	{
+		a *= 2;
+	}
+
+	return a;
+}
+
+int main()
+{
+	engine::Ref<Object> obj = new Object();
+	
+	engine::ElapsedTimer timer;
+	timer.reset();
+
+	int a = doSomeStuff();
+
+	float deltaTime = timer.getTime();
+
+	printf("%d, deltaTime=%2.2f\n", a, deltaTime);
+
+	return 0;
+}
+
